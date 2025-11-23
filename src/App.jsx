dd8df02df9c73fb5f816dce441437b6ffc58ab6f
keamoMichael <michaelkeamo@gmail.com>
2025-11-23 12:52:30 +0200

    Set login page as default route

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,9 @@ export default function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Signup />} />
-                <Route path="/signup" element={<Signup />} />
+                <Route path="/" element={<Login />} />
                 <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<Signup />} />
                 <Route path="/dashboard" element={<Dashboard />} />
             </Routes>
         </BrowserRouter>
